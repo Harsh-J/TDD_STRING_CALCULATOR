06bfe39a87eae6dc80dec6ddc7627843ca848d1b
Harsh <harshjagarwal123@gmail.com>
2024-06-12 05:48:15 -1200

    test case for string with one element

--- test.js
+++ test.js
@@ -8,4 +8,9 @@ describe("Task addition", () => {
   it("Calculate empty string", () => {
     assert.strictEqual(stringCalcObj.calculate(""), 0);
   });
+
+  it("Calculate string with one element", () => {
+    assert.strictEqual(stringCalcObj.calculate("1"), 1);
+  });
+
 });
