652a9ab3b9194608aadbcb523cdd7d5534e07de0
Harsh <harshjagarwal123@gmail.com>
2024-06-12 05:50:41 -1200

    test case for string with two elements

--- test.js
+++ test.js
@@ -15,5 +15,11 @@ describe("Task addition", () => {
   it("Calculate string with one element 2", () => {
     assert.strictEqual(stringCalcObj.calculate("5"), 5);
   });
+  it("Calculate string with one element 3", () => {
+    assert.strictEqual(stringCalcObj.calculate("7"), 7);
+  });
+  it("Calculate string with two elements", () => {
+    assert.strictEqual(stringCalcObj.calculate("1,1"), 2);
+  });
 
 });
